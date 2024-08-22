8ca931bcd0d57776358ca3ed1567812647e03708
xiaoyeaichif <1125035561@qq.com>
2024-08-23 01:04:03 +0800

    函数模板学习

--- 常见算法的汇总/04function.cpp
+++ 常见算法的汇总/04function.cpp
@@ -12,8 +12,12 @@ int add(int a, int b)
 int main()
 {
 	// function<int(int, int)>的别名是func1
+	// 返回值为int,参数列表为int，int
 	using func1 = function<int(int, int)>;
 	func1 fun = add;
 	cout << fun(10, 20) << endl;
+	// 方式2
+	function<int(int, int)> func2;
+	cout << func2(10, 20) << endl;
 	return 0;
 }
